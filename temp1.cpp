84111c283ae35ebfec098b3b610241caa365f761
Rishabh Gupta <rishabhgp06@gmail.com>
2026-06-26 07:29:58 +0530

    12002

--- temp1.cpp
+++ temp1.cpp
@@ -1,50 +1,41 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-#ifndef ONLINE_JUDGE
-	freopen("Error.txt", "w", stderr);
-#endif
-
-	int n, q;
-	cin >> n >> q;
-
-	// Vector to store the first position of each color, initialized to n+1
-	vector<int> first_pos(51, n + 1);
-
-	// Reading the deck and storing the first occurrence of each color
-	for (int i = 1; i <= n; i++) { // Loop through each card in the deck
-		int color;
-		cin >> color;
-		// If this is the first occurrence of the color, store its position
-		if (first_pos[color] == n + 1) {
-			first_pos[color] = i;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		vector<pair<int, int>> a(n), b(n), c(n);
+		for(int i =0;i<n;i++){
+			cin>>a[i].first;
+			a[i].second = i;
 		}
-	}
-
-	// Process each query
-	while (q--) { // Loop through each query
-		int color;
-		cin >> color;
-		// Get the position of the first occurrence of the queried color
-		int ans = first_pos[color];
-
-		// Update the positions of all colors that are above the queried color
-		for (int i = 1; i <= 50; i++) {
-			if (first_pos[i] < ans) {
-				first_pos[i]++;
+		for(int i =0;i<n;i++){
+			cin>>b[i].first;
+			b[i].second = i;
+		}
+		for(int i =0;i<n;i++){
+			cin>>c[i].first;
+			c[i].second = i;
+		}
+		sort(a.rbegin(),a.rend());
+		sort(b.rbegin(),b.rend());
+		sort(c.rbegin(),c.rend());
+		int maxf =0;
+		for(int i =0;i<3;i++){
+			for(int j =0;j<3;j++){
+				for(int k =0;k<3;k++){
+					int x = a[i].second;
+					int y = b[j].second;
+					int z = c[k].second;
+					
+					if(x!=y && y!=z && x!=z){
+						maxf = max(a[i].first+b[j].first+c[k].first,maxf);
+					}
+				}
 			}
 		}
-
-		// Move the queried color to the top of the deck
-		first_pos[color] = 1;
-
-		// Output the position of the queried color
-		cout << ans << " ";
+		cout<<maxf<<"\n";
 	}
-
-	cout << endl;
-
-	// Time Complexity (TC): O(N + Q * 50)
-	// Space Complexity (SC): O(50)
 }
