fbe86c9892c211a11c0f85f94154c7141af046a7
Rishabh Gupta <rishabhgp06@gmail.com>
2026-06-28 11:59:15 +0530

    cp

--- temp1.cpp
+++ temp1.cpp
@@ -6,9 +6,11 @@ int main(){
 	while(t--){
 		long long n,x,y;
 		cin>>n>>x>>y;
-		int q = n/x;
-		int w = n/y;
+		long long lcm_xy = (x / __gcd(x, y)) * y;
+		long long gc = n / lcm_xy;
+		long long q = n/x - gc;
+		long long w = n/y - gc;
 		long long ans = n*(n+1)/2 - (n-q)*((n-q)+1)/2 - w*(w+1)/2;
-		cout<<ans<<endl;
+		cout<<ans<<"\n";
 	}
 }
