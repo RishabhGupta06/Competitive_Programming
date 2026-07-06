1360866f26f44846bb5d2cadc8f73516bf4c7407
Rishabh Gupta <rishabhgp06@gmail.com>
2026-07-06 23:30:03 +0530

    q

--- solve.cpp
+++ solve.cpp
@@ -3,28 +3,7 @@
 using namespace std;
 
 int main() {
-	int t;
-	cin>>t;
-	while(t--){
-		int n,m;
-		cin>>n>>m;
-		vector<vector<int>> arr(n,vector<int>(m) );
-		for(int i =0;i<n;i++){
-			for (int j = 0; j < m; j++)
-			{
-				cin>>arr[j][i];
-			}
-		}
-		for(int i =0;i<m;i++){
-			sort(arr[i].begin(),arr[i].end());
-		}
-		int ans =0;
-		for(int i =0;i<m;i++){
-			for(int j=0;j<n;j++){
-				ans -= (arr[i][j] * (n - j - 1));
-				ans += (arr[i][j] * j);
-			}
-		}
-		cout<<ans<<endl;
-	}
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
 }
