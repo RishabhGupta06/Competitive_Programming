17171f8a55968e999820d5422804dbbcb272577a
Rishabh Gupta <rishabhgp06@gmail.com>
2026-07-17 23:42:13 +0530

    Update:

--- solve.cpp
+++ solve.cpp
@@ -1,31 +1,33 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-void solve() {
-    int n;
-    cin>>n;
-    vector<int> arr(n);
-    for(int i =0;i<n;i++) cin>>arr[i];
-    long long sum =0;
-    long long zero =0;
-    bool flag = false;
-    for(int i = 0;i<n-1;i++){
-        if(arr[i] > 0) flag = true;
-        if(arr[i] == 0 && flag) zero++;
-        else sum += arr[i];
+int main(){
+    long long n,m;
+    cin>>n>>m;
+    vector<long long> arr(n+1);
+    vector<long long > pre(n+1);
+    vector<long long> suff(n+1);
+    pre[0] = 0;
+    pre[1] = 0;
+    arr[0] = INT_MAX;
+    cin>>arr[1];
+    for(int i =2;i<n+1;i++){
+        cin>>arr[i];
+        if(arr[i]<arr[i-1]) pre[i] = abs(arr[i]-arr[i-1])+pre[i-1];
+        else pre[i] = pre[i-1];
     }
-    cout<<zero+sum<<"\n";
-}
+    suff[suff.size()-1] = 0;
+    for(int i =suff.size()-2;i>=1;i--){
+        if(arr[i]<arr[i+1]) suff[i] = abs(arr[i]-arr[i+1])+suff[i+1];
+        else suff[i] = suff[i+1];
 
-int main() {
-    // Fast I/O for Codeforces
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
     }
-    return 0;
+
+    for(int i =0;i<m;i++){
+        int a,b;
+        cin>>a>>b;
+        if(a<b){
+            cout<<pre[b] - pre[a]<<endl;
+        }
+        else cout<<suff[b] - suff[a]<<endl;
+    }
 }
