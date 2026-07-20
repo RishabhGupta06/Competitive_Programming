95dbd690a0628af7530d2f352fca3d869e7e4c11
Rishabh Gupta <rishabhgp06@gmail.com>
2026-07-20 22:22:50 +0530

    u

--- solve.cpp
+++ solve.cpp
@@ -2,29 +2,25 @@
 using namespace std;
 
 void solve() {
-    long long a, b, c, d;
-    cin >> a >> b >> c >> d;
-    
-    // Cross multiply to avoid decimal/integer division issues
-    long long x = a * d;
-    long long y = b * c;
-    
-    // Case 0: Already equal
-    if (x == y) {
-        cout << 0 << "\n";
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int ones = 0;
+    int flips = 0;
+    for (char c : s) {
+        if (c == '1') {
+            ones++;
+        } else if (ones > 0) {
+            flips++;
+            if (flips > ones) {
+                flips = ones;
+            }
+        }
     }
-    // Case 1: One of them is 0 (we just multiply the other's numerator by 0)
-    else if (x == 0 || y == 0) {
-        cout << 1 << "\n";
-    }
-    // Case 1 (continued): One cross-product is a multiple of the other
-    else if (x % y == 0 || y % x == 0) {
-        cout << 1 << "\n";
-    }
-    // Case 2: Everything else takes exactly 2 claps
-    else {
-        cout << 2 << "\n";
-    }
+
+    cout << flips << '\n';
 }
 
 int main() {
