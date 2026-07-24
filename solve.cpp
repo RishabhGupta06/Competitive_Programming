f2c58d8849818b95024afa1f3b421421f6e4aad2
Rishabh Gupta <rishabhgp06@gmail.com>
2026-07-24 17:28:11 +0530

    u

--- solve.cpp
+++ solve.cpp
@@ -2,31 +2,6 @@
 using namespace std;
 
 void solve() {
-    int a,b;
-    cin>>a>>b;
-    string s;
-    cin>>s;
-    vector<int> le(26,0);
-    for(int i =0;i<s.size();i++){
-        le[s[i] - 97]++;
-    }
-    if(le[0] == 0) for(int i =0;i<b;i++) cout<<'a';
-    else{
-        char c;
-        int j =0;
-        for (int i = 1; i <= b; i++)
-        {
-            int j =0;
-            for(j;j<a/b;j++){
-                if(le[j] != 0){
-                    le[j]--;
-                }
-                else break;
-            }
-            cout<<char('a'+j);
-        }
-    }
-    cout<<endl;
 
 }
 
