0162ec742af04054e543b4ffe9652914cdeeca45
Rishabh Gupta <rishabhgp06@gmail.com>
2026-08-07 22:53:49 +0530

    update

--- solve.cpp
+++ solve.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 void solve() {
     int n;
-    cin >> n;
-    
-    // Calculate the number of swaps needed
-    int m = (n + 1) / 2;
-    cout << m << "\n";
-    
-    // Left pointer starts at the first 'B' (index 1)
-    int left = 1;
-    
-    // Right pointer starts at the last 'N' (index 3*n)
-    int right = 3 * n;
-    
-    // Perform exactly 'm' swaps
-    for (int i = 0; i < m; i++) {
-        cout << left << " " << right << "\n";
-        
-        // Move to the next 'B' (jump over 'A' and 'N')
-        left += 3;
-        
-        // Move backward to the next 'N' (jump over 'A' and 'B')
-        right -= 3;
+    cin>>n;
+    vector<int> time(n);
+    vector<int> spell(n);
+    vector<pair<int,int>> r;
+    for(int i =0;i<n;i++) cin>>time[i];
+    for(int i =0;i<n;i++) cin>>spell[i];
+    for(int i =0;i<n;i++) r.push_back({spell[i],i});
+    sort(r.begin(),r.end());
+    int t =0;
+    for(int i =0;i<n;i++){
+        t += time[r[i].second];
+        int x = r[i].second;
+        if(x == 0){
+            time[1] += r[i].first;
+        }
+        else if(x == n-1) time[n-2] += r[i].first;
+        else {
+            time[x+1] += r[i].first;
+            time[x-1] += r[i].first;
+        }
     }
+    cout<<t<<"\n";
 }
 
 int main() {
