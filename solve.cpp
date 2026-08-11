95de78668b15010d2bd7876fa92ee5154d15e462
Rishabh Gupta <rishabhgp06@gmail.com>
2026-08-11 07:25:14 +0530

    update

--- solve.cpp
+++ solve.cpp
@@ -3,31 +3,10 @@
 using namespace std;
 
 void solve() {
-    int n;
-    cin>>n;
-    vector<int> arr(n);
-    for(int i =0;i<n;i++) cin>>arr[i];
-
-    sort(arr.begin(),arr.end());
-    if (arr[0] == arr[n - 1]) {
-        // Cast to long long to prevent overflow: 10^5 * 10^5 = 10^10
-        cout << (long long)n * (n - 1) << "\n";
-        return; 
-    }
-    long long first = 0,second =0;
-    int i =0, j =n-1;
-    while(arr[i] == arr[0] || arr[j] == arr[n-1]){
-        if(arr[j] == arr[n-1]){
-            second++;
-            j--;
-        }
-        if(arr[i] == arr[0]){
-            first++;
-            i++;
-        }
-    }
-
-     cout<<(first*second)*2<<endl;
+    string s;
+    cin>>s;
+    if(s[1] == 'a') cout<<s[0]<<" "<<s[1]<<" "<<s.substr(2)<<endl;
+    else cout<<s[0]<<" "<<s.substr(1,s.size()-2)<<" "<<s[s.size() -1]<<endl;
 }
 
 int main() {
