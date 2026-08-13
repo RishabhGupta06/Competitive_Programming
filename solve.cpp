ba0df9d4cdf7b211f34f6c605e57a1c25f0a748f
Rishabh Gupta <rishabhgp06@gmail.com>
2026-08-13 23:13:56 +0530

    update

--- solve.cpp
+++ solve.cpp
@@ -2,23 +2,46 @@
 #define ll long long
 using namespace std;
 
-void solve() {
+void solve()
+{
     long long n;
-    cin>>n;
-    long long x = (n*(n-1))%1000000007;
-    long long fact =1;
-    for (int i = 1; i <= n; i++)
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
     {
-        fact =( fact*i)%1000000007;
+        cin >> arr[i];
     }
-    cout<<(fact*x)%1000000007<<endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        int low = 1;
+        int high = i + 1;
+        int best_d = 1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (arr[i - mid + 1] >= mid)
+            {
+                best_d = mid;
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        cout<<best_d<<" ";
+    }
+    cout<<"\n";
 }
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
     // return 0;
