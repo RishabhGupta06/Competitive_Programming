99f50a8d45c020e2d565aa568987f32fca3c3b2e
Rishabh Gupta <rishabhgp06@gmail.com>
2026-08-15 23:57:54 +0530

    update

--- solve.cpp
+++ solve.cpp
@@ -1,39 +1,59 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-void solve()
-{
-    int n,q;
-    cin>>n>>q;
-    vector<long long> arr(n+1);
-    vector<long long> pre(n+1,0);
-    for(int i =1;i<=n;i++){
-        cin>>arr[i];
-
-         pre[i] = pre[i-1] + arr[i];
-        
+// Helper function to calculate the luckiness of a single number
+int getLuckiness(int n) {
+    int max_digit = 0;
+    int min_digit = 9;
+    
+    // Extract digits one by one
+    while (n > 0) {
+        int digit = n % 10;
+        max_digit = max(max_digit, digit);
+        min_digit = min(min_digit, digit);
+        n /= 10;
     }
-
-    while(q--){
-        long long l,r,k;
-        cin>>l>>r>>k;
-        long long sum = pre[n];
-        sum -= (pre[r]-pre[l-1]);
-        sum += (r-l+1)*k;
-        if(sum%2 == 0) cout<<"No"<<endl;
-        else cout<<"Yes"<<endl;
-    }
-
+    
+    return max_digit - min_digit;
 }
 
-int main()
-{
+void solve() {
+    int l, r;
+    cin >> l >> r;
+    
+    int best_number = l;
+    int best_score = -1;
+
+    for (int i = l; i <= r; i++) {
+        int current_score = getLuckiness(i);
+        
+        if (current_score > best_score) {
+            best_score = current_score;
+            best_number = i;
+        }
+        
+        // THE MAGIC SPEED BOOST: 
+        // 9 is the absolute maximum possible luckiness (9 - 0 = 9).
+        // Since we are guaranteed to find a 9 within any 100 consecutive numbers,
+        // we can stop searching immediately to prevent Time Limit Exceeded!
+        if (best_score == 9) {
+            break; 
+        }
+    }
+    
+    cout << best_number << "\n";
+}
+
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         solve();
     }
-    // return 0;
+    
+    return 0;
 }
