d5c7e0e79217e180032113817bd50190eaef4225
Rishabh Gupta <rishabhgp06@gmail.com>
2026-08-24 21:53:50 +0530

    update

--- temp.cpp
+++ temp.cpp
@@ -1,10 +1,21 @@
 #include<bits/stdc++.h>
-#define ll long long int
 using namespace std;
+
 int main(){
     int t;
-    cin>>t;
+    cin >> t;
     while(t--){
+        long long n; // Use long long for 10^18
+        cin >> n;
         
+        // We look for the first number that DOES NOT divide n
+        // because the longest interval always starts at 1.
+        for(long long i = 1; ; i++){
+            if(n % i != 0){
+                cout << i - 1 << endl;
+                break; // We found the break in the chain, output and move to next test case
+            }
+        }
     }
+    return 0;
 }
