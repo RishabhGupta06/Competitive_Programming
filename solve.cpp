10e98cc3622ee0d34b95e6dd14e53446f7e070e0
Rishabh Gupta <rishabhgp06@gmail.com>
2026-08-29 13:00:33 +0530

    update

--- solve.cpp
+++ solve.cpp
@@ -1,58 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// DFS function to calculate the balance of each subtree
-int dfs(int node, const vector<vector<int>>& adj, const string& colors, int& balanced_count) {
-    int current_balance = 0;
-    
-    // 1. Calculate the score of the current node's color
-    // Note: 'colors' string is 0-indexed, but our nodes are 1-indexed (1 to n)
-    if (colors[node - 1] == 'W') {
-        current_balance += 1;
-    } else {
-        current_balance -= 1;
+int dfs(int node, vector<vector<int>> &x, string &col, int &ans){
+    int temp =0;
+
+    if(col[node-1] == 'W') temp++;
+    else temp--;
+
+    int no_child = x[node].size();
+
+    for(int i =0;i<no_child;i++){
+        int child = x[node][i];
+
+        temp += dfs(child,x,col,ans);
     }
-    
-    // 2. Ask all children for their balance scores and add them up
-    for (int child : adj[node]) {
-        current_balance += dfs(child, adj, colors, balanced_count);
-    }
-    
-    // 3. If the total score of this subtree is 0, it has an equal number of W and B!
-    if (current_balance == 0) {
-        balanced_count++;
-    }
-    
-    // 4. Return this subtree's total score up to its parent
-    return current_balance;
+    if(temp == 0) ans++;
+    return temp;
 }
 
-void solve() {
+void solve(){
     int n;
-    cin >> n;
-    
-    // Create an adjacency list for 1-based indexing
-    vector<vector<int>> adj(n + 1);
-    
-    // Read the parent array (starts giving parents for vertex 2)
-    for (int i = 2; i <= n; i++) {
-        int parent;
-        cin >> parent;
-        adj[parent].push_back(i); 
+    cin>>n;
+    vector<vector<int>> x(n+1);
+    for(int i =2;i<=n;i++){
+        int z;
+        cin>>z;
+        x[z].push_back(i);
     }
-    
-    // Read the colors of the vertices
-    string colors;
-    cin >> colors;
-    
-    // Track the total number of perfectly balanced subtrees
-    int balanced_count = 0;
-    
-    // Start the DFS from the root of the tree (Node 1)
-    dfs(1, adj, colors, balanced_count);
-    
-    // Print the final answer for this test case
-    cout << balanced_count << "\n";
+
+    string col;
+    cin>>col;
+
+    int ans =0;
+    dfs(1, x, col, ans);
+
+    cout<<ans<<endl;
 }
 
 int main() {
