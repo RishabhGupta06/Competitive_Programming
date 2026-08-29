b0cf0b88b8347e95713ee1325d9ee6091562130f
Rishabh Gupta <rishabhgp06@gmail.com>
2026-08-30 00:44:55 +0530

    update

--- solve.cpp
+++ solve.cpp
@@ -1,40 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dfs(int node, vector<vector<int>> &x, string &col, int &ans){
-    int temp =0;
-
-    if(col[node-1] == 'W') temp++;
-    else temp--;
-
-    int no_child = x[node].size();
-
-    for(int i =0;i<no_child;i++){
-        int child = x[node][i];
-
-        temp += dfs(child,x,col,ans);
-    }
-    if(temp == 0) ans++;
-    return temp;
-}
-
 void solve(){
-    int n;
-    cin>>n;
-    vector<vector<int>> x(n+1);
-    for(int i =2;i<=n;i++){
-        int z;
-        cin>>z;
-        x[z].push_back(i);
+    int n,m;
+    cin>>n>>m;
+    vector<int> arr(n);
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
     }
 
-    string col;
-    cin>>col;
-
-    int ans =0;
-    dfs(1, x, col, ans);
-
-    cout<<ans<<endl;
+    vector<int> fr(m+2,0);
+    for(int i =0;i<n;i++){
+        fr[arr[i]]++;
+    }
+    
+    vector<int> suff(m+2,0);
+    for(int i = m-1;i>0;i--){
+        suff[i] = suff[i+1]+fr[i+1];
+    }
+    int max_ans =0;
+    for(int i = 1;i<=m;i++){
+        max_ans = max((fr[i]+suff[i]+(2*i <= m ? fr[2*i] :0)),max_ans);
+    }
+    cout<<max_ans<<endl;
 }
 
 int main() {
