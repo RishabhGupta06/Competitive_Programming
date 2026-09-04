2233c7570010dfbcdb75c28a9057b67305f402da
Rishabh Gupta <rishabhgp06@gmail.com>
2026-09-04 13:47:24 +0530

    update

--- solve.cpp
+++ solve.cpp
@@ -2,25 +2,7 @@
 using namespace std;
 
 void solve(){
-    int n,k,m;
-    cin>>n>>k>>m;
-
-    int window = 1;
-    int sum =0;
-    if(m<k) cout<<"No"<<endl;
-    else{
-        cout<<"Yes"<<endl;
-        int sum =0;
-        for(int i =1;i<=n;i++){
-            if(i%k == 0){
-                cout<<m - (k-1)<<" ";
-            }
-            else{
-                cout<<1<<" ";
-            }
-        }
-        cout<<endl;
-    }
+    int n;
 }
 
 int main() {
