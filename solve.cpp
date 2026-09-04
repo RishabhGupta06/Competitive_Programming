097ca7ffc228b509633e3ac3a9ce8114b589a115
Rishabh Gupta <rishabhgp06@gmail.com>
2026-09-04 18:36:49 +0530

    update

--- solve.cpp
+++ solve.cpp
@@ -1,14 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long lcm(long long a, long long b) {
+    return (a / __gcd(a, b)) * b;  // safer against overflow
+}
+
+// Function to compute LCM of numbers from 1 to n
+long long lcmFrom1ToN(int n) {
+    long long result = 1;
+    for (int i = 2; i <= n; i++) {
+        result = lcm(result, i);
+    }
+    return result;
+}
 void solve(){
     int n;
     cin>>n;
-    long long prod = 1;
-    int gcdi = 0;
-    for(int i =1;i<=n;i++){ prod *= i;
-    gcdi = __gcd(gcdi,i);
+ int lcm = lcmFrom1ToN(n) ;
+ int sum =0;
+for(int i =1;i<n;i++){
+    cout<<i<<" ";
+    sum += i;
 }
+    
+cout<<lcm - sum<<endl;
 
 
 }
