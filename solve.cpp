774fa5316b6cff52aabefb295d04fd7601bfa40b
Rishabh Gupta <rishabhgp06@gmail.com>
2026-09-09 00:14:30 +0530

    Updated Code

--- solve.cpp
+++ solve.cpp
@@ -3,27 +3,16 @@ using namespace std;
 
 
 void solve(){
-    int n;
-    cin>>n;
-
-
-    int n1 =0;
-    int n2 =0;
-    int n3 =0;
-
-
-    for(int i =0;i<n;i++){
-        int x;
-        cin>>x;
-
-        if(x%2 != 0){
-            n1++;
-        }
-        else if(x%4 == 0) n3++;
-        else n2++;
-    }
-
-    cout<<max({n1,n2,n3})<<endl;
+    long long n,m,a;
+    cin>>n>>m>>a;
+    long long y =0;
+    int x =0;
+    if(n%a != 0) x += n/a+1;
+    else x += n/a;
+    if(m%a != 0) y += m/a+1;
+    else y += m/a;
+    
+    cout<<x*y<<endl;
 
 }
 
@@ -32,11 +21,11 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
-    while (t--) {
+    // int t;
+    // cin >> t;
+   // while (t--) {
         solve();
-    }
+    //}
     
     return 0;
 }
