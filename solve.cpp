6413632aad8c0612477d3efa3a6c0111d5b557d4
Rishabh Gupta <rishabhgp06@gmail.com>
2026-09-19 14:44:33 +0530

    Updated Code

--- solve.cpp
+++ solve.cpp
@@ -6,24 +6,15 @@ void solve(){
     int n,k;
     cin>>n>>k;
 
-    int even =0;
-    int ans =INT_MAX;
-    for(int i =0;i<n;i++){
-        int x;
-        cin>>x;
-
-        if(x%2 ==0) even++;
-
-        if(x%k ==0) ans =0;
-        else{
-            ans = min(ans,k - (x%k));
-        }
+    if(k != 10){
+        for(int i = 1;i<=n ;i++) cout<<k;
     }
-
-    if(k == 4){
-        ans = min(ans, max(0,2-even));
+    else if( n == 1) cout<< -1;
+    else{
+        cout<<1;
+        for(int i =1;i<n;i++) cout<<0;
     }
-    cout<<ans<<endl;
+    cout<<endl;
 }
 
 int main() {
@@ -31,11 +22,11 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
-   while (t--) {
+    // int t;
+    // cin >> t;
+//    while (t--) {
         solve();
-    }
+    // }
     
     return 0;
 }
